ae1d7205d3ae72ecec88c9511f3cd83906fcdb47
Divyanshi Purohit <purohitdivyanshi25@gmail.com>
2026-07-04 22:51:19 +0530

    added the vector store and its functions

---
+++ core/vector_store.py
@@ -0,0 +1,66 @@
+import os 
+from langchain_chroma import Chroma 
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+
+CHROMA_DIR = "vector_db"
+COLLECTION_NAME = "meeting_transcript"
+EMBEDDING_MODEL  = "all-MiniLM-L6-v2"
+
+
+#  Function: Initialize HuggingFace embeddings 
+def get_embeddings():
+    return HuggingFaceEmbeddings(
+        model_name = EMBEDDING_MODEL,
+        model_kwargs = {"device" : 'cpu'}
+    )
+
+'''
+Function: Build a new Chroma vector store from transcript
+ - Splits transcript into chunks (500 chars, 50 overlap)
+ - Wraps each chunk in a Document with metadata
+ - Embeds chunks and persists them in Chroma DB
+'''
+def build_vector_store(transcript : str)->Chroma:
+    print("Building vector Store")
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 500,
+        chunk_overlap = 50
+    )
+    chunks = splitter.split_text(transcript)
+
+    docs = [
+        Document(page_content=chunk, metadata = {'chunk_index' : i})
+        for i,chunk in enumerate(chunks)
+    ]
+
+    embeddings = get_embeddings()
+    vector_store = Chroma.from_documents(
+        documents= docs,
+        embedding=embeddings,
+        collection_name=COLLECTION_NAME,
+        persist_directory=CHROMA_DIR
+    )
+
+    return vector_store
+
+
+# Function: Load an existing Chroma vector store
+def load_vector_store() ->Chroma:
+    embeddings = get_embeddings()
+    vector_store = Chroma(
+        collection_name=COLLECTION_NAME,
+        embedding_function= embeddings,
+        persist_directory=CHROMA_DIR
+    )
+
+    return vector_store
+
+# Function: Get k most relevant chunks from vector store
+def get_retriever(vector_store : Chroma, k :int = 4):
+    return vector_store.as_retriever(
+        search_type = 'similarity',
+        search_kwargs = {"k":k}
+    )
